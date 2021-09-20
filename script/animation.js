bd5543f185ac52be4101e568c85cc8fb6228f140
mikeuseni <77750051+Mike1908@users.noreply.github.com>
2021-09-20 14:48:15 -0400

    init

---
+++ script/animation.js
@@ -0,0 +1,17 @@
+let animB = document.querySelector(".navbarrClas");
+let animBText= document.querySelector(".Logo a img");
+
+//console.log(animB);
+window.addEventListener('scroll',() =>{
+        if (window.scrollY > 30) {
+            animB.classList.add('anim-navbarr');
+            animBText.classList.add('LogoDef');
+            //console.log(animBText);
+        } else {
+            animB.classList.remove('anim-navbarr');
+            animBText.classList.remove('LogoDef');
+
+            //console.log(animB)
+        }
+        //console.log(navigation);
+    })
